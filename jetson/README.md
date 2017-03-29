39644570885a2de8376974b55b35bce9f71cda49
Alexandra Haraszti <ajharaszti@gmail.com>
2017-03-29 01:37:57 -0700

    Filter out Jetson builtin camera and allow remote restart
    The remote restart is actually achieved in conjunction with the systemd
    service script which will indeed restart the camera server app any time it
    detects that it has exited.
    
    See the jeston/README.md file for further details and usage info.

--- jetson/README.md
+++ jetson/README.md
@@ -1,1 +1,47 @@
 ## Code used on the Jetson TX1
+
+The actual script serving the camera streams is cameraserver.py.
+
+The additional systemd service definition file can be used to
+make the cameraserver to be a system service, so that it can
+be auto-started, can do failure recovery, and even allow the
+cameraserver.py to be restarted remotely from the FRC Dashboard
+using "Button 3".
+
+The remote restart is actually achieved in conjunction with the systemd
+service script: activating Button 3 on the dashboard will make the
+cameraserver.py loop to break and hence cameraserver.py to exit.
+The systemd script will then attempt to restart the cameraserver (since
+it detects that it has died). Note that while Button 3 is active, the
+cameraserver.py process will keep exiting. So, in order to allow it to
+properly run, Button 3 must be toggled back to "Off" position.
+
+For the above to work, the cameraserver.service systemd file needs
+to be copied into /lib/systemd/system and then enabled. The actual
+steps:
+
+```
+sudo cp lib-systemd-system/cameraservice.service /lib/systemd/system
+sudo chmod 644 /lib/systemd/system/cameraserver.service
+sudo systemctl daemon-reload
+sudo systemctl enable cameraserver.service
+```
+
+After reboot, the cameraserver.py will now be auto-started.
+
+Some useful commands:
+
+To show status of cameraserver, including the last many lines
+of its log:
+
+```
+systemctl status cameraserver.service
+```
+
+To stop/start/restart cameraserver manually:
+
+```
+sudo systemctl stop cameraserver.service
+sudo systemctl start cameraserver.service
+sudo systemctl restart cameraserver.service
+```
--- jetson/cameraserver.py
+++ jetson/cameraserver.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from time import time
+from time import sleep
 from cscore import CameraServer
 from networktables import NetworkTables
+from os import system
 
 def main():
 
     # Get ready with default camera and enable cscore logginf
     cs = CameraServer.getInstance()
     cs.enableLogging()
-    
+ 
+    # Find usable cameras (those which support YUYV mode)
+    camera_ids = []
+    for id in range(3):
+        if system('v4l2-ctl --all -d /dev/video%s | grep "Pixel Format" | grep RG10' % id) != 0:
+            camera_ids.append(id)
+
+    # Check that we found at least two usable cameras
+    assert len(camera_ids) >= 2
+
     # Enable capturing (waiting for a streaming client to connect)
-    camera0 = cs.startAutomaticCapture(dev=0, name='Jetson Camera 0')
-    camera1 = cs.startAutomaticCapture(dev=1, name='Jetson Camera 1')
+    camera0 = cs.startAutomaticCapture(dev=camera_ids[0], name='Jetson Camera 0')
+    camera1 = cs.startAutomaticCapture(dev=camera_ids[1], name='Jetson Camera 1')
     # cs.startAutomaticCapture(dev=2, name='Jetson Camera 2')
 
     camera0.setResolution(160, 120)
@@ -29,6 +39,17 @@ def main():
     sd = NetworkTables.getTable("SmartDashboard/DB") 
 
     while True:
+
+        # Allow Button 3 (from Dashboard) state change to exit,
+        # thus forcing a restart
+        if sd.getBoolean("Button 3", False):
+            # wait a bit, then set button back to Off
+            sleep(0.5)
+            sd.putBoolean("Button 3", True)
+            sleep(0.5)
+            break
+
+        # Allow Button 0 (from Dashboard) to select camera source
         if sd.getBoolean("Button 0", False):
             sink = cvSink0
         else:
